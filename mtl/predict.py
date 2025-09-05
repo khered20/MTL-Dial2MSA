9ef2d3f04d2efe032ef85774e2c6950dcac9271f
khered20 <khered20@gmail.com>
2025-09-06 00:27:43 +0100

    update

--- mtl/predict.py
+++ mtl/predict.py
@@ -42,3 +42,23 @@ def predict(model, tokenizer, texts, device, max_length=128, num_beams=3):
         for inp, tr, di in zip(texts, translations, dialects):
             results.append({"input": inp, "translation": tr, "dialect": di})
     return results
+
+
+def BigListPred(model, test_loader, tokenizer, device): 
+    model.eval() 
+    translations, class_preds = [], [] 
+    with torch.no_grad(): 
+        for batch in test_loader: 
+            batch = {k: v.to(device) for k, v in batch.items()} 
+            if 'T5' in model.__class__.__name__: 
+                outputs = model.translator.generate(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], max_length=128, num_beams=3) 
+                class_logits = model.classifier(model.translator.encoder(batch['input_ids'], attention_mask=batch['attention_mask']).last_hidden_state[:, 0, :]) 
+            else: 
+                outputs = model.translator.generate(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], # forced_bos_token_id=tokenizer.convert_tokens_to_ids("ar_AR"), 
+                                                    max_length=128, num_beams=3) 
+                class_logits = model.classifier(model.translator(input_ids=batch['input_ids'], attention_mask=batch['attention_mask']).encoder_last_hidden_state[:, 0, :]) 
+            translations.extend([tokenizer.decode(ids, skip_special_tokens=True) for ids in outputs]) 
+                # Classification predictions 
+            class_preds.extend(torch.argmax(class_logits, dim=1).cpu().numpy())
+    class_preds = [id_to_dialect_label(int(x)) for x in class_preds] 
+    return translations, class_preds
